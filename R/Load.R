414ed38b42febeace2b7051007e4f334cab890ed
Duncan Temple Lang <duncan@r-project.org>
2021-12-31 10:07:53 -0800

    implement of the Load() function. Need to fix the vars and ...

---
+++ R/Load.R
@@ -0,0 +1,18 @@
+Load =
+function(file, envir = parent.frame(), verbose = FALSE, vars = as.character(list(...) ), ...)
+{
+    if(length(vars) == 0)
+        return(invisible(load(file, envir, verbose)))
+
+    e = new.env()
+    load(file, e, verbose)
+
+    new.vars = names(vars)
+    if(length(new.vars) == 0) 
+        names(vars) = new.vars = vars
+    else if(any(w <- (names(vars) == "")))
+         new.vars[w] = vars[w]        
+
+    mapply(function(v1, v2) assign(v1, get(v2, e), envir), new.vars, vars)
+    invisible(vars)
+}
