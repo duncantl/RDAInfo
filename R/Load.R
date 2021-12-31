b0ad7424c2bb7a47cb08324778944718001f8b29
Duncan Temple Lang <duncan@r-project.org>
2021-12-31 10:13:44 -0800

    better way of handling vars and ... in Load(). Still need to decide whether to switch the order of vars and ... in the formals()

--- R/Load.R
+++ R/Load.R
@@ -1,12 +1,15 @@
 Load =
-function(file, envir = parent.frame(), verbose = FALSE, vars = as.character(list(...) ), ...)
+function(file, envir = parent.frame(), verbose = FALSE, vars = character(), ...)
 {
+    vars = c(vars, unlist(list(...)))
+    
     if(length(vars) == 0)
         return(invisible(load(file, envir, verbose)))
 
     e = new.env()
     load(file, e, verbose)
 
+    
     new.vars = names(vars)
     if(length(new.vars) == 0) 
         names(vars) = new.vars = vars
@@ -16,3 +19,5 @@ function(file, envir = parent.frame(), verbose = FALSE, vars = as.character(list
     mapply(function(v1, v2) assign(v1, get(v2, e), envir), new.vars, vars)
     invisible(vars)
 }
+
+
