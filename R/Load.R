1ce367462158f8e4b81a89345630bf7db8c40f59
Duncan Temple Lang <duncan@r-project.org>
2022-01-02 11:31:23 -0800

    removed ... from Load() and wrote documentation and an example

--- R/Load.R
+++ R/Load.R
@@ -8,25 +8,41 @@ Load =
     # This does read and restore all of the variables in the Rda file and discards the ones you
     # don't want to keep.
     #
-function(file, envir = parent.frame(), verbose = FALSE, vars = character(), ..., .noOverwrite = TRUE)
+function(file, envir = parent.frame(), verbose = FALSE, vars = character(), .noOverwrite = TRUE)
 {
-    vars = c(vars, unlist(list(...)))
+    # vars = c(vars, unlist(list(...)))
     
     if(length(vars) == 0)
         return(invisible(load(file, envir, verbose)))
 
-    e = new.env()
+    e = new.env(parent = emptyenv())
     load(file, e, verbose)
 
+    inRDA = sapply(vars, exists, e, inherits = FALSE)
+    if(any(!inRDA)) {
+        warning( paste(vars[!inRDA], collapse = ", "), " not found in RDA stream", file)
+        vars = vars[inRDA]
+    }
     
     new.vars = names(vars)
     if(length(new.vars) == 0) 
         names(vars) = new.vars = vars
     else if(any(w <- (names(vars) == "")))
          new.vars[w] = vars[w]        
-
-    mapply(function(v1, v2) if(.noOverwrite && !exists(v1, envir)) assign(v1, get(v2, e), envir), new.vars, vars)
-    invisible(vars)
+    
+    
+    ok = mapply(doAssign, vars, new.vars, MoreArgs = list(from = e, to = envir, .noOverwrite = .noOverwrite))
+    
+    vars[is.na(ok) | ok]
 }
 
+doAssign =
+function(var, newVar, from, to, .noOverwrite = TRUE)
+{
+    if(!.noOverwrite || !exists(newVar, to, inherits = FALSE)) {
+        assign(newVar, get(var, e), to)
+        TRUE
+    } else
+        FALSE
+}
 
