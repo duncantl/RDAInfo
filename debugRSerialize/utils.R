89687dd56447f29df7894dd8158439021d47b4cc
Duncan Temple Lang <duncan@r-project.org>
2023-11-06 08:20:16 -0800

    avoid loading any packages

--- debugRSerialize/utils.R
+++ debugRSerialize/utils.R
@@ -1,17 +1,18 @@
 # source("RDAXDR/R/SEXPMap.R")
-library(RDAInfo)
-library(RJSONIO)
+#library(RDAInfo)
+#library(RJSONIO)
+
 readJS =
 function(file = "/tmp/foo.json")
 {
-    df = as.data.frame(do.call(rbind, fromJSON(file)))
+    df = as.data.frame(do.call(rbind, RJSONIO::fromJSON(file)))
     names = c("type", "depth", "hastag", "hasattr")
     if(length(df) == 6)
         names = c(names, c("objf", "levs"))
     names(df) = names
     df[] = lapply(df[], as.integer)
     df[3:length(df)] = lapply(df[3:length(df)], as.logical)    
-    df$typeName = sexpType(df$type)
+    df$typeName = RDAInfo:::sexpType(df$type)
     df
 }
 
